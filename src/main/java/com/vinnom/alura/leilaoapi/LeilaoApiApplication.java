9c58213f09effe9f28cabf60e9208b8a98a1f01d
vinnom <vinnom.2d@gmail.com>
2020-09-10 15:24:09 -0300

    commit inicial

---
+++ src/main/java/com/vinnom/alura/leilaoapi/LeilaoApiApplication.java
@@ -0,0 +1,13 @@
+package com.vinnom.alura.leilaoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeilaoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeilaoApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vinnom/alura/leilaoapi/LeilaoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinnom.alura.leilaoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeilaoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
